2b49aa5cebeb7f6613c51486dd00d28325dd31bd
Aaron Day <daya@crazydays.org>
2013-08-17 10:06:37 -0400

    Initial commit.

---
+++ 2013/stripe_demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StripeDemo::Application.config.secret_key_base = 'e8322c24235aef2ac8c202fe33b5d9b42fac2ec7ce6e7910647084e5247ee7d3c7aa7fbc2bec1044b04df6dc558a274cc16f3ee13e019db05640b400fe51cc87'
